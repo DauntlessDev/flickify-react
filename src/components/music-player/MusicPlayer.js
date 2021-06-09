fa453bce718ffed6a40be0df3ca31e988f78d36e
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2021-06-09 19:57:26 +0800

    fixed links

--- src/components/music-player/MusicPlayer.js
+++ src/components/music-player/MusicPlayer.js
@@ -9,9 +9,9 @@ export default function MusicPlayer() {
       name: "Despacito",
       singer: "Luis Fonsi",
       cover:
-        "http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg",
+        "https://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg",
       musicSrc:
-        "http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3",
+        "https://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3",
       // support async fetch music src. eg.
       // musicSrc: async () => {
       //   return await fetch('/api')
